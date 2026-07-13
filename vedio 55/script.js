10ad32631b9f12eb616384ea395f12e731110c34
Pushkar8002 <pushkarsingh8002@gmail.com>
2026-07-13 13:18:25 +0530

    Add files via upload

---
+++ vedio 55/script.js
@@ -0,0 +1,35 @@
+console.log("hey this is tutorial 55 of javascript  ");
+
+let a = 45;
+let b = 56;
+// var 66a = 67; // This will throw an error because variable names cannot start with a number 
+let c = "this is a string";
+console.log(a+b);
+console.log(c);
+
+const author = "harry";
+// author = "rohan"; // This will throw an error because const variables cannot be reassigned
+console.log(author);
+{
+let a = 11;
+console.log(a);
+}
+console.log(a);
+
+let x = "this is a string";
+let y = 45;
+let z = true;
+const p = null;
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
+console.log(typeof p);
+
+let o = {
+    name: "harry",
+    age: 22,
+    isStudent: true
+};
+console.log(o);
+o.name = "rohan";
+console.log(o);
