4afdd9d82355a2ff9953a959779673ceeff10edc
Pushkar8002 <pushkarsingh8002@gmail.com>
2026-07-13 13:18:47 +0530

    Add files via upload

---
+++ vedio 56/script.js
@@ -0,0 +1,8 @@
+console.log("Hello, World! This is a simple JavaScript file.");
+
+let age = 25;
+if (age >= 18) {
+    console.log("You are an adult.");
+}else {
+    console.log("You are a minor.");
+}
