667863dadf83c5d5a0fce24b135b3eecd0fccbd1
Pushkar8002 <pushkarsingh8002@gmail.com>
2026-07-13 13:21:45 +0530

    Add files via upload

---
+++ vedio 68/script.js
@@ -0,0 +1,12 @@
+console.log("PUshkar singh")
+
+let box = document.getElementsByClassName("box");
+console.log(box);
+
+box[0].style.backgroundColor = "red";
+box[1].style.backgroundColor = "green";
+box[2].style.backgroundColor = "blue";
+box[3].style.backgroundColor = "yellow";
+box[4].style.backgroundColor = "cyan";
+
+
