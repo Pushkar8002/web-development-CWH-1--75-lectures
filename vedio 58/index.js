cc048612577f2b9c324a6a05c946deb6eddb3cf7
Pushkar8002 <pushkarsingh8002@gmail.com>
2026-07-13 13:19:41 +0530

    Add files via upload

---
+++ vedio 58/index.js
@@ -0,0 +1,13 @@
+console.log("java script functions");
+function add(a, b) {
+    return a + b;
+}
+console.log(add(5, 3));
+function subtract(a, b) {
+    return a - b;
+}
+console.log(subtract(5, 3));
+// arrow function
+const multiply = (a, b) => {
+    return a * b;
+}
